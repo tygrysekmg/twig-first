50a5421d95226f3dacd406771e10ec4a23c758e4
root <root@grzegorz-php72.com>
2018-03-14 18:24:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# twig-first
+in file index.php add require_once '/path/to/vendor/autoload.php';
+
+install twig in folder composer require "twig/twig:^2.0"
+if is all right you see image like tw.png
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once '/opt/lampp/htdocs/twig/vendor/autoload.php';
+
+$loader = new Twig_Loader_Array(array(
+    'index' => 'Hello {{ name }}!',
+));
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index', array('name' => 'Fabien'));
+?>
